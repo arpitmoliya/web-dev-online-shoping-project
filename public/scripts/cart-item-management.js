60f6a3ff737a2acf76f7ca369568f9cd25e76ea3
arpitmoliya <arpitmoliya153@gmail.com>
2022-04-10 11:01:58 +0530

    add for lop for badge element

--- public/scripts/cart-item-management.js
+++ public/scripts/cart-item-management.js
@@ -2,7 +2,7 @@ const cartUpdateFormElements = document.querySelectorAll(
   ".cart-item-management"
 );
 const cartTotalPriceElement = document.getElementById("cart-total-price");
-const cartBadge = document.querySelector(".nav-items .badge");
+const cartBadgeElements = document.querySelectorAll(".nav-items .badge");
 
 async function updateCartItem(event) {
   event.preventDefault();
@@ -50,7 +50,9 @@ async function updateCartItem(event) {
   cartTotalPriceElement.textContent =
     responseData.updateCartData.newTotalPrice.toFixed(2);
 
-  cartBadge.textContent = responseData.updateCartData.newTotalQuantity;
+  for (const cartBadgeElement of cartBadgeElements) {
+    cartBadgeElement.textContent = responseData.updateCartData.newTotalQuantity;
+  }
 }
 
 for (const formElement of cartUpdateFormElements) {
