daec2093c1d58fe447378603e6913ad84add13cb
arpitmoliya <arpitmoliya153@gmail.com>
2022-04-10 10:49:15 +0530

    add more methods for finding order and updating order

--- models/order.model.js
+++ models/order.model.js
@@ -1,5 +1,7 @@
 const db = require("../database/database");
 
+const mongodb = require("mongodb");
+
 class Order {
   // Status => pending, fullfilled,cancelled
   constructor(cart, userData, status = "pending", date, orderId) {
@@ -18,9 +20,61 @@ class Order {
     this.id = orderId;
   }
 
+  static transformOrderDocument(orderDoc) {
+    return new Order(
+      orderDoc.productData,
+      orderDoc.userData,
+      orderDoc.status,
+      orderDoc.date,
+      orderDoc._id
+    );
+  }
+
+  static transformOrderDocuments(orderDocs) {
+    return orderDocs.map(this.transformOrderDocument);
+  }
+
+  static async findAll() {
+    const orders = await db
+      .getDb()
+      .collection("orders")
+      .find()
+      .sort({ _id: -1 })
+      .toArray();
+
+    return this.transformOrderDocuments(orders);
+  }
+
+  static async findAllForUser(userId) {
+    const uid = new mongodb.ObjectId(userId);
+
+    const orders = await db
+      .getDb()
+      .collection("orders")
+      .find({ "userData._id": uid })
+      .sort({ _id: -1 })
+      .toArray();
+
+    return this.transformOrderDocuments(orders);
+  }
+
+  static async findById(orderId) {
+    const order = await db
+      .getDb()
+      .collection("orders")
+      .findOne({ _id: new mongodb.ObjectId(orderId) });
+
+    return this.transformOrderDocument(order);
+  }
+
   save() {
     if (this.id) {
       //Updating
+      const orderId = new mongodb.ObjectId(this.id);
+      return db
+        .getDb()
+        .collection("orders")
+        .updateOne({ _id: orderId }, { $set: { status: this.status } });
     } else {
       const orderDocument = {
         userData: this.userData,
